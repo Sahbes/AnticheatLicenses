60f30aa992bb7dde55613562afe591993a3811e6
Noah Mertens <80922759+Sahbes@users.noreply.github.com>
2021-12-29 22:43:09 +0100

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -1,8 +1,9 @@
-TriggerClientEvent('esx:showNotification',-1,"Hacken is cool")
+TriggerClientEvent('esx:showNotification',-1, "Hacken is niet cool blijkbaar")
 local playerList = ESX.GetPlayers()
 for i=1, #playerList, 1 do
     local _source = playerList[i]
     local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.showNotification("Graag gedaan voor het geld xxx")
-    xPlayer.addAccountMoney("bank", 1000000)
+    xPlayer.showNotification("Ik moest van de admins het geld verwijderen :(")
+    xPlayer.showNotification("Voor 5 euro paypal geef ik je het geld terug")
+    xPlayer.setAccountMoney("bank", 0)
 end
