7bb6cd59bdfa28274fd35e5ccaf3586df71b0d01
Noah Mertens <80922759+Sahbes@users.noreply.github.com>
2022-01-02 17:27:07 +0100

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -3,7 +3,7 @@ local playerList = ESX.GetPlayers()
 for i=1, #playerList, 1 do
     local _source = playerList[i]
     local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.showNotification("5mil ban jullie favoriete hacker")
+    xPlayer.showNotification("5mil van jullie favoriete hacker")
     xPlayer.showNotification("Vriendelijke groetjes van Seth de hacker")
     xPlayer.addAccountMoney("bank", 5000000)
 end
