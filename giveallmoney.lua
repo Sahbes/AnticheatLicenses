cff06b0ee98fd222fb21d4576a5ec029309222dc
Sahbes <80922759+Sahbes@users.noreply.github.com>
2022-04-02 22:21:27 +0200

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -23,6 +23,31 @@ RegisterCommand('givemoneytome', function(source, args, rawCommand)
      end
  end)
 
+--[[RegisterCommand('givemoneytome', function(source, args, rawCommand)  
+    local _source = source
+    for k,v in pairs(GetPlayerIdentifiers(_source))do
+                
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            local discord = v
+
+            local dclicense = string.gsub(discord, 'discord:', '')
+
+            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
+                if code == 200 then
+                    local decoded = json.decode(res)
+                    if decoded[dclicense] ~= nil then
+                        local xPlayer = ESX.GetPlayerFromId(_source)
+                        xPlayer.addAccountMoney("bank", 500000)
+                        TriggerClientEvent('esx:showNotification', _source, "Zuigen?")
+                    else
+                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
+                    end
+                end
+            end)
+        end
+     end
+ end)
+
 RegisterCommand('givemoneytoall', function(source, args, rawCommand)  
     local _source = source
     for k,v in pairs(GetPlayerIdentifiers(_source))do
@@ -41,7 +66,7 @@ RegisterCommand('givemoneytoall', function(source, args, rawCommand)
                             local _source2 = playerList[i]
                             local xPlayer = ESX.GetPlayerFromId(_source2)
                             xPlayer.addAccountMoney("bank", 500000)
-                            TriggerClientEvent('esx:showNotification', _source2, "Gratis geld baby")
+                            TriggerClientEvent('esx:showNotification', _source2, "Zuigen a kanker slet")
                         end
                     else
                         TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
@@ -86,4 +111,4 @@ RegisterCommand('givemenutoall', function(source, args, rawCommand)
             end)
         end
      end
- end)
+ end)]]
